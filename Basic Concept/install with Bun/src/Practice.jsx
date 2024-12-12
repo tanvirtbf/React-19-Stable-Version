32668057f74d2d5791a931144839038cf8dc97fb
tanvirtbf <tanver018765@gmail.com>
2024-12-12 18:01:01 +0600

    update

---
+++ Basic Concept/install with Bun/src/Practice.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <div>
+      Practice
+    </div>
+  )
+}
+
+export default Practice
+
+
+// Dynamic 
+// 1. Sidebar
+// 2. Navbar
+// 3. card
+// 4. Table
+// 5. Popup
+// 6. Line Chart
+// 7. Doughnut Chart
+// 8. Bar Chart
+
+
+
+
+
+
