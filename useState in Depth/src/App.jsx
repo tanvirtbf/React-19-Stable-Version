f2a71b3d6884d9d6242ce38074baff3dfd7edaab
tanvirtbf <tanver018765@gmail.com>
2025-01-07 16:36:11 +0600

    useRef

--- useState in Depth/src/App.jsx
+++ useState in Depth/src/App.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
 
-  function handleClick(){
-    const num1 = document.getElementById('num1')
-    const num2 = document.getElementById('num2')
+  const num1Ref = useRef(null);
+  const num2Ref = useRef(null);
 
-    setValue(Number(num1.value)+Number(num2.value))
+  function handleClick() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
 
-    num1.value = null
-    num2.value = null
+    // setValue(Number(num1.value) + Number(num2.value));
+    setValue(Number(num1Ref.current.value) + Number(num2Ref.current.value))
+
+    num1.value = null;
+    num2.value = null;
   }
-  
 
   return (
     <>
       <div>
-        <input type="text" id='num1' />
-        <input type="text" id='num2' />
+        <input ref={num1Ref} type="text" id="num1" />
+        <input ref={num2Ref} type="text" id="num2" />
         <button onClick={handleClick}>ADD</button>
       </div>
       <h1>{value}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
