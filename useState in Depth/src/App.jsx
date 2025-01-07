e878f6ff34f61f5392ebf83cadab8eef4297f9a6
tanvirtbf <tanver018765@gmail.com>
2025-01-08 02:22:11 +0600

    no value change===no re-render test

--- useState in Depth/src/App.jsx
+++ useState in Depth/src/App.jsx
@@ -2,27 +2,13 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(0);
 
-  const num1Ref = useRef(null);
-  const num2Ref = useRef(null);
-
-  function handleClick() {
-    const num1 = document.getElementById("num1");
-    const num2 = document.getElementById("num2");
-
-    // setValue(Number(num1.value) + Number(num2.value));
-    setValue(Number(num1Ref.current.value) + Number(num2Ref.current.value))
-
-    num1.value = null;
-    num2.value = null;
-  }
 
   return (
     <>
       <div>
-        <input ref={num1Ref} type="text" id="num1" />
-        <input ref={num2Ref} type="text" id="num2" />
+        <input type="text" id="num1" />
+        <input type="text" id="num2" />
         <button onClick={handleClick}>ADD</button>
       </div>
       <h1>{value}</h1>
