7501c7dcb7531425d1f6c73bc977f48e2a56389a
tanvirtbf <tanver018765@gmail.com>
2025-01-10 04:49:03 +0600

    update

--- Calculator App/src/calculator/index.jsx
+++ Calculator App/src/calculator/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './style.css'
 import Input from './components/input'
+import Button from './components/button'
 
 const Calculator = () => {
 
     function onInput(value){
         console.log(value)
     }
+    function handleClick(){
+      console.log('HandleClick!')
+    }
 
   return (
     <div>
         <Input placeholder='Enter Number' onInput={onInput} />
+        <Button label='Add' onClick={handleClick} />
     </div>
   )
 }
