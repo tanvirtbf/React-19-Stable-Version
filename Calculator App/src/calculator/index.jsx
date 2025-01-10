59a815957702cfd686fcc3e4c9a5d9cbee49d6a7
tanvirtbf <tanver018765@gmail.com>
2025-01-10 22:10:57 +0600

    all button created

--- Calculator App/src/calculator/index.jsx
+++ Calculator App/src/calculator/index.jsx
@@ -3,7 +3,17 @@ import './style.css'
 import Input from './components/input'
 import Button from './components/button'
 
+const Add = "+"
+const Subtract = "-"
+const Divide = "/"
+const Multiply = "*"
+const Equal = "="
+
+const Operations = [Add, Subtract, Divide, Multiply, Equal]
+const Nums = [...new Array(10)]
+
 const Calculator = () => {
+  
 
     function onInput(value){
         console.log(value)
@@ -15,7 +25,17 @@ const Calculator = () => {
   return (
     <div>
         <Input placeholder='Enter Number' onInput={onInput} />
-        <Button label='Add' onClick={handleClick} />
+        <div>
+          {Nums.map((_, index)=> {
+            return <Button key={index} label={index} onClick={handleClick} />
+          })}
+        </div>
+        <div>
+          {Operations.map(o => {
+            return <Button key={o} label={o} />
+          })}
+        </div>
+        
     </div>
   )
 }
