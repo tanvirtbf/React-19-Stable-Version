2ea4d2c6ff719fb24dfc2aecde5424f9b2a1e8ff
tanvirtbf <tanver018765@gmail.com>
2025-01-11 10:29:45 +0600

    calculator app

--- Calculator App/src/calculator/index.jsx
+++ Calculator App/src/calculator/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import "./style.css";
 import Input from "./components/input";
 import Button from "./components/button";
