25de4dbe935bb9e97a636a6db9dd224f749505db
tanvirtbf <tanver018765@gmail.com>
2025-01-11 18:08:30 +0600

    update

--- Word Guessing Game/src/word-guessing-game/keyboard/index.jsx
+++ Word Guessing Game/src/word-guessing-game/keyboard/index.jsx
@@ -6,6 +6,7 @@ function Keybaord({handleKeyPress}) {
     const arr = [...new Array(26)];
     let start = 65;
 
+    
     function handleClick(key) {
         return ()=> {
             handleKeyPress(key);
