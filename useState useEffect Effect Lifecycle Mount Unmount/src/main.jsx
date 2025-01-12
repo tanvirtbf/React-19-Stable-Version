8c8cd4e64139a4e046ff44579cf46fcf5faaa3e1
tanvirtbf <tanver018765@gmail.com>
2025-01-13 01:08:28 +0600

    update

--- useState useEffect Effect Lifecycle Mount Unmount/src/main.jsx
+++ useState useEffect Effect Lifecycle Mount Unmount/src/main.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
+
+
+
+
+
+
