a902eca5b9afc9266af0328a092aedaf5426c81f
tanvirtbf <tanver018765@gmail.com>
2025-01-15 17:42:19 +0600

    object test

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TestUseEffect from "./components/TestUseEffect";
 
 function App() {
   const [count, setCount] = useState(0)
-  function xyz(){
-    console.log('Hiii')
-  }
+
+  const [obj, setObj] = useState({
+    name : 'Tanvir',
+    age : 25, 
+    roll : 1,
+  })
+
+  if(count % 3 === 0) obj.roll = count
+
+  useEffect(()=>{
+    console.log('hello')
+  }, [obj])
+
   return (
     <div>
       <button onClick={()=> setCount(count+1)}>Click Me</button>
