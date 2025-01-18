3b0ac46545bf76803f9e8a437a17aa40db6d0d5b
tanvirtbf <tanver018765@gmail.com>
2025-01-18 08:52:36 +0600

    update

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  console.log('App Render!')
   const [count, setCount] = useState(0)
-
-  const arr = []
+  const [msg, setMessage] = useState('No Message!')
 
   useEffect(()=>{
-    console.log('hello')
-  }, [arr.length])
+    if(count === 2) {
+      setMessage("Hello World!")
+    }
+  }, [count])
 
   return (
     <div>
       <button onClick={()=> setCount(count+1)}>Click Me {count}</button>
+      <div>{msg}</div>
     </div>
   );
 }
