be329c601ec58d7d2451618026437dc50fe631cb
tanvirtbf <tanver018765@gmail.com>
2025-01-18 17:06:31 +0600

    ui complete for testing

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -1,26 +1,48 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-// Reseting States when Props change example :
 
-function Child({id}){
-  console.log('Child Render!')
-  const [childCount, setChildCount] = useState(0)
+function App(){
 
-  return <button onClick={()=> setChildCount(childCount+1)}>{childCount}</button>
-}
+  const [formData, setFormData] = useState({
+    firstName : '',
+    lastName : '',
+    email : '',
+    password : '',
+  })
 
-function App() {
-  console.log('App Render!')
-  const [count, setCount] = useState(0)
+  function handleFname(e){
+    setFormData({...formData, firstName: e.target.value})
+  }
+  function handleLname(e){
+    setFormData({...formData, lastName: e.target.value})
+  }
+  function handleEmail(e){
+    setFormData({...formData, email: e.target.value})
+  }
+  function handlePassword(e){
+    setFormData({...formData, password: e.target.value})
+  }
 
-  function handleClick(){
-    setCount(count + 1)
+  function handleSubmit(e){
+    e.preventDefault()
+    setFormData({firstName:'',lastName:'',email:'',password:''})
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Click Me {count}</button>
-      <Child key={count} id={count} />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleFname} type="text" placeholder="first name" />
+        <input onChange={handleLname} type="text" placeholder="last name" />
+        <input onChange={handleEmail} type="email" placeholder="email" />
+        <input onChange={handlePassword} type="password" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h5>First Name : {formData.firstName}</h5>
+        <h5>Last Name : {formData.lastName}</h5>
+        <h5>Email : {formData.email}</h5>
+        <h5>Password : {formData.password}</h5>
+      </div>
     </div>
   );
 }
