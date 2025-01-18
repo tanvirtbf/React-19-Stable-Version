bc098e6de7cb9458ffad7de9b0ec6c4272e4df7e
tanvirtbf <tanver018765@gmail.com>
2025-01-18 09:21:41 +0600

    third way for don't use useEffect

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -4,14 +4,11 @@ import "./App.css";
 function App() {
   console.log('App Render!')
   const [count, setCount] = useState(0)
-  const [msg, setMessage] = useState('No Message!')
+
+  const msg = count%2==0 ? 'Yes' : 'No'
 
   function handleClick(){
-    const newCount = count + 1
-    setCount(count+1)
-    if(newCount===3){
-      setMessage('Hello World!')
-    }
+    setCount(count + 1)
   }
 
   return (
