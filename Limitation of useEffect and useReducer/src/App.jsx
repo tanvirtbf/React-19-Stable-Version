236048cfb40e39650a31766640e08e248489c3fd
tanvirtbf <tanver018765@gmail.com>
2025-01-18 14:08:31 +0600

    update

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+function getVisibleTodos(num){
+  return num+10
+}
 
 function App() {
   console.log('App Render!')
-  const [count, setCount] = useState(0)
-
-  const msg = count%2==0 ? 'Yes' : 'No'
+  const [newTodo, setNewTodo] = useState('')
+  const [visibleTodos, setVisibleTodos] = useState('')
 
   function handleClick(){
     setCount(count + 1)
+    setVal(getValue(val))
   }
+
 
   return (
     <div>
       <button onClick={handleClick}>Click Me {count}</button>
-      <div>{msg}</div>
+      <div>{val}</div>
     </div>
   );
 }
