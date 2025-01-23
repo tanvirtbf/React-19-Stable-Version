1ea14ac760f3a4c685d081a200479f18387f5598
tanvirtbf <tanver018765@gmail.com>
2025-01-23 15:38:37 +0600

    inline function grav with memo

--- useMemo useCallback ProviderContext CustomHooks/src/App.jsx
+++ useMemo useCallback ProviderContext CustomHooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TestMemo from "./components/TestMemo";
 
@@ -13,9 +13,17 @@ function App(){
     <div>
       <button onClick={handleClick}>Count {count}</button>
       <TestMemo />
+      <MemoizedComponent />
     </div>
   );
 }
 
+const MemoizedComponent = React.memo(()=>{
+  console.log('Memoized Component Render')
+  return <h1>Hello World!</h1>
+})
+
 export default App;
+
+
 
