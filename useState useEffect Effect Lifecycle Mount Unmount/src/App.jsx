e4125a995c87256a4a50080764cabd00bc81a1af
tanvirtbf <tanver018765@gmail.com>
2025-05-13 16:55:23 +0600

    component mount serial test

--- useState useEffect Effect Lifecycle Mount Unmount/src/App.jsx
+++ useState useEffect Effect Lifecycle Mount Unmount/src/App.jsx
@@ -1,22 +1,51 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Parent from "./components/Parent";
 
 function App() {
-  const [select, setSelect] = useState(true)
+  const [count, setCount] = useState(0)
+
+  function handleIncrease(){
+    setCount(count+1)
+    setCount(count+10)
+    setCount(count+1)
+    // setCount((prev) => prev+10)
+    // setCount((prev) => prev+10)
+    setCount(count+1)
+    console.log(count);
+  }
 
   useEffect(() => {
-    console.log("App Mount!");
-    return () => {
-      console.log("App Unmount!");
-    };
-  }, []);
+    console.log('App Component Mount!');
+  }, [])
+
   return (
     <div>
-      <button onClick={()=> setSelect(!select)}>Unmount Parent</button>
-      {select && <Parent />}
+      <Child />
+      <button onClick={handleIncrease}>Increase {count}</button>
     </div>
   );
 }
+
+function Child(){
+  useEffect(() => {
+    console.log('Child Component Mount!');
+  }, [])
+  return (
+    <h1>
+      <GrandChild />
+      Hello Child!</h1>
+  )
+}
+
+function GrandChild(){
+  useEffect(() => {
+    console.log('Grand Child Mount!');
+  }, [])
+
+  return (
+    <h1>Grand Child</h1>
+  )
+}
+
 
 export default App;
