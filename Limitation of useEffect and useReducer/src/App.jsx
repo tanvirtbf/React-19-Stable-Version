e1c6885d76770c973a975a0826d1f7a1f6b0423b
tanvirtbf <tanver018765@gmail.com>
2025-05-14 18:16:00 +0600

    update

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -1,29 +1,35 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+// when count >3 , immidiate call APP
+// setMsg schedule and dom update
+// when ui mount then setMsg run and msg updated and APP Re-render again
+// Then Again App mount with dom update
 
 function App(){
   const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState("No Message!")
 
   function handleClick(e){
     setCount(count+1)
   }
 
-  const getData = useCallback(() => {
-    console.log('Get Data Called!');
-  }, [])
-
   useEffect(() => {
-    getData();
-  }, [getData])
+    if(count>3){
+      setMsg("Hello World!")
+    }
+  }, [count])
 
   return (
     <div>
-      <button onClick={handleClick}>Count {count}</button>
+      <button onClick={handleClick}>{msg} {count}</button>
     </div>
   );
 }
 
 export default App;
+
+// dependency array te amake sob somoy premitive value dite hobe . non-premitive sob somoy avoid korte hobe . jodi function array object amake use korte hoy tahole amake value gula derrived korte hobe . nahole amake react.memo or callback use kore kaj korte hobe . jate amake oproyojoni call ke thamaite pari 
 
 
 
