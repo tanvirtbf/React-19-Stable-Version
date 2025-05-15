e232266ac54c523a5a4a9e648b35ee889ecdcf87
tanvirtbf <tanver018765@gmail.com>
2025-05-15 18:25:01 +0600

    useMemo and useCallback

--- useMemo useCallback ProviderContext CustomHooks/src/App.jsx
+++ useMemo useCallback ProviderContext CustomHooks/src/App.jsx
@@ -1,42 +1,44 @@
-import React, { memo, useEffect, useRef, useState } from "react";
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 
-function App(){
-  const [count, setCount] = useState(0)
-  const [msg, setMsg] = useState("")
-  const countRef = useRef(null)
+function App() {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(null);
 
-  function handleClick(){
-    setCount(count+1)
-    countRef.current = count+1
-  }
+  const handleClick = useCallback(() =>  {
+    setCount((prev) => prev + 1);
+    countRef.current = count + 1;
+  }, [])
 
   useEffect(() => {
     let intervalId = setInterval(() => {
-      setCount(countRef.current+3)
-    }, 5000)
+      setCount(countRef.current + 3);
+    }, 5000);
 
     return () => {
-      clearInterval(intervalId)
-    }
-  }, [])
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div>
-      <MemoizedChild msg={msg} />
+      <Child handleClick={handleClick} />
       <button onClick={handleClick}>Count {count}</button>
     </div>
   );
 }
 
-function Child(){
-  console.log('Child Component run!');
-  return <h1>Hello Child!</h1>
-}
+const Child = memo(({ handleClick }) => {
+  console.log("Child Component run!");
 
-const MemoizedChild = memo(Child)
+  // Expensive Logic
+  useMemo(() => {
+    for (let i = 0; i < 10; i++) {
+      console.log(`Logic Running ${i + 1}`);
+    }
+  }, []);
+
+  return <h1>Hello Child!</h1>;
+});
 
 export default App;
-
-
-
