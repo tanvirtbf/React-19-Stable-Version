488987e89e20c7e577ae2306be6f3387a7103132
tanvirtbf <tanver018765@gmail.com>
2025-05-16 00:44:40 +0600

    api call code separate in custom hooks name useNetworkCall

--- useMemo useCallback ProviderContext CustomHooks/src/App.jsx
+++ useMemo useCallback ProviderContext CustomHooks/src/App.jsx
@@ -8,46 +8,33 @@ import React, {
   useSyncExternalStore,
 } from "react";
 import "./App.css";
+import { useNetworkCall } from "./hooks/useNetworkCall";
 
 const API_URL = "https://dummyjson.com/products";
 
+
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const {data, loading, error} = useNetworkCall(API_URL)
 
-  useEffect(() => {
-    setLoading(true);
-    setError("");
-
-    setTimeout(() => {
-      fetch(API_URL)
-        .then((res) => res.json())
-        .then((d) => {
-          setData(d.products);
-        })
-        .catch((err) => {
-          setError("Some Error Found!");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }, 5000);
-  }, []);
-
-  if(loading){
-    return <h1>Loading...</h1>
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
-  if(error){
-    return <h1>{error}</h1>
+  if (error) {
+    return <h1>{error}</h1>;
   }
 
-  if(data.length===0){
-    return <h1>Data Not Found!</h1>
+  if (data.length === 0) {
+    return <h1>Data Not Found!</h1>;
   }
 
-  return <div>{data.map((item) => <p key={item.id}>{item.title}</p>)}</div>;
+  return (
+    <div>
+      {data.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
