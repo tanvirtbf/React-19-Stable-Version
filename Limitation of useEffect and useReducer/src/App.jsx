507e3867c1e7926909a03727601764e049041426
tanvirtbf <tanver018765@gmail.com>
2025-05-15 13:16:51 +0600

    reducer function setup

--- Limitation of useEffect and useReducer/src/App.jsx
+++ Limitation of useEffect and useReducer/src/App.jsx
@@ -1,33 +1,65 @@
-import { useEffect, useState, useSyncExternalStore } from "react";
+import { useEffect, useReducer, useState, useSyncExternalStore } from "react";
 import "./App.css";
 
+function reducerFn(state, action){
+  if(action.type==='FNAME'){
+    return {...state, fName: action.payload}
+  }else if(action.type==='LNAME'){
+    return {...state, lName: action.payload}
+  }else if(action.type==='EMAIL'){
+    return {...state, email: action.payload}
+  }else {
+    return state
+  }
+}
+
 function App(){
-  const [count, setCount] = useState(0)
+  const [form, dispatch] = useReducer(reducerFn, {
+    fName: "", 
+    lName: "",
+    email: ""
+  })
 
-  function subscribe(){
-    document.addEventListener("click", handleOutsideClick)
-
-    return () => {
-      document.removeEventListener("click", handleOutsideClick)
-    }
+  function handleFName(e){
+    dispatch({type: "FNAME", payload: e.target.value})
   }
-
-  useSyncExternalStore(subscribe, ()=>{})
-
-
-  function handleOutsideClick(){
-    console.log('Click on Document!');
+  function handleLName(e){
+    dispatch({type: "LNAME", payload: e.target.value})
   }
-
-  function handleClick(e){
-    setCount(count+1)
+  function handleEmail(e){
+    dispatch({type: "EMAIL", payload: e.target.value})
   }
 
   return (
     <div>
-      <button onClick={handleClick}>{count}</button>
+      <input type="text" placeholder="Enter First Name : " value={form.fName} onInput={handleFName} />
+      <input type="text" placeholder="Enter Last Name : " value={form.lName} onInput={handleLName} />    
+      <input type="text" placeholder="Enter Email : " value={form.email} onInput={handleEmail} />    
+      <button>Show Message</button>
+      <div>
+        <div>{form.fName}</div>
+        <div>{form.lName}</div>
+        <div>{form.email}</div>
+      </div>
     </div>
   );
+}
+
+function Child(){
+
+  useEffect(() => {
+
+    setInterval(() => {
+      console.log('Inside Child Component');
+    })
+
+    document.addEventListener("click", () => {
+      console.log("Clicked!");
+    })
+
+  }, [])
+
+  return <h1>I am Child Component</h1>
 }
 
 
