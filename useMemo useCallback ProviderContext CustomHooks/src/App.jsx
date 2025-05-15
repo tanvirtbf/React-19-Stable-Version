5c72f5c47d662e0238918a0fab999c76a9d27e5d
tanvirtbf <tanver018765@gmail.com>
2025-05-15 22:02:10 +0600

    memo, useMemo, useCallback test

--- useMemo useCallback ProviderContext CustomHooks/src/App.jsx
+++ useMemo useCallback ProviderContext CustomHooks/src/App.jsx
@@ -1,32 +1,38 @@
-import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import React, {
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
 
-function App(){
-  const [count, setCount] = useState(0)
+function App() {
+  const [count, setCount] = useState(0);
 
-  function handleClick(){
-    setCount(count+1)
-  }
+  const handleClick = useCallback(()=> {
+    setCount((prev) => prev + 1);
+  }, [])
 
   return (
     <div>
-      <Child count={count} />
+      <Child handleClick={handleClick} />
       <button onClick={handleClick}>Count {count}</button>
     </div>
-  )
+  );
 }
 
-const Child = memo(({ count }) => {
+const Child = memo(({ handleClick }) => {
+  console.log("Child Component Re-render!");
 
-  console.log('Child Component Re-render!');
-
-  return <h1>Child Component!</h1>
-}, (prevProps, nextProps) => {
-    if(nextProps.count > 10){
-      return false
-    } else {
-      return true
+  useMemo(() => {
+    for (let i = 0; i < 10; i++) {
+      console.log(`Render ${i + 1}`);
     }
-})
+  }, [handleClick]);
+
+  return <h1>Child Component!</h1>;
+});
 
 export default App;
