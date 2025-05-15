85e21a370511b5a3994514640c1030affa37662d
tanvirtbf <tanver018765@gmail.com>
2025-05-15 17:56:33 +0600

    useRef usecase clear

--- useMemo useCallback ProviderContext CustomHooks/src/App.jsx
+++ useMemo useCallback ProviderContext CustomHooks/src/App.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-import TestMemo from "./components/TestMemo";
 
 function App(){
   const [count, setCount] = useState(0)
+  const countRef = useRef(null)
 
-  function handleClick(e){
+  function handleClick(){
     setCount(count+1)
+    countRef.current = count+1
   }
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setCount(countRef.current+3)
+    }, 5000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
 
   return (
     <div>
       <button onClick={handleClick}>Count {count}</button>
-      <TestMemo />
-      <MemoizedComponent />
     </div>
   );
 }
-
-const MemoizedComponent = React.memo(()=>{
-  console.log('Memoized Component Render')
-  return <h1>Hello World!</h1>
-})
 
 export default App;
 
