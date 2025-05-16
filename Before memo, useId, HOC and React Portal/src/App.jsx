88b619bddab5886cee7fd003bcf5ba69b8e9f022
tanvirtbf <tanver018765@gmail.com>
2025-05-16 23:15:00 +0600

    start testing with context provider

--- Before memo, useId, HOC and React Portal/src/App.jsx
+++ Before memo, useId, HOC and React Portal/src/App.jsx
@@ -9,29 +9,5 @@ function App() {
   );
 }
 
-function Form({ children }) {
-  const [color, setColor] = useState("");
-
-  return (
-    <div style={{ backgroundColor: color }}>
-      <input
-        style={{ backgroundColor: color }}
-        type="text"
-        value={color}
-        onInput={(e) => setColor(e.target.value)}
-      />
-      {children}
-    </div>
-  );
-}
-
-function Child() {
-  console.log("Rendering Child");
-  return (
-    <div>
-      <h1>Child</h1>
-    </div>
-  );
-}
 
 export default App;
