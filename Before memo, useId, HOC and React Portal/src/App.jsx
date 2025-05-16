4ac54619e1256fccc53771634342a4df91deadb3
tanvirtbf <tanver018765@gmail.com>
2025-05-16 23:20:51 +0600

    udpate

--- Before memo, useId, HOC and React Portal/src/App.jsx
+++ Before memo, useId, HOC and React Portal/src/App.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { memo, useContext, useState } from "react";
 import "./App.css";
 
+const MyContext = React.createContext(null);
+
 function App() {
+  const [state, setState] = useState(0)
   return (
-    <Form>
+    <MyContext.Provider value={{ state, setState}}>
+      <GrandChild />
       <Child />
-    </Form>
+    </MyContext.Provider>
   );
 }
+
+const Child = () => {
+  const {state, setState} = useContext(MyContext)
+
+  return (
+    <div>
+      {state}
+      <h2 onClick={() => setState((prev) => prev+1)}>Child Component</h2>
+    </div>
+  )
+}
+
+const GrandChild = memo( () => {
+  console.log('Grand Child Rendered');
+  return (
+    <div>
+      <h2>Grand Child Component</h2>
+    </div>
+  )
+})
 
 
 export default App;
