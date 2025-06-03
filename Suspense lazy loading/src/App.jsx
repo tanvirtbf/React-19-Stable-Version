b6e25afe25fcdf96aa642eaf68e5137154ef00ed
tanvirtbf <tanver018765@gmail.com>
2025-06-03 11:04:08 +0600

    App ui complete

--- Suspense lazy loading/src/App.jsx
+++ Suspense lazy loading/src/App.jsx
@@ -1,11 +1,26 @@
 import React, { memo, useContext, useState } from "react";
 import "./App.css";
+import Parent from "./components/Parent";
+import Child from "./components/Child";
 
 const App = () => {
+  const [showParent, setShowParent] = useState(false)
+  const [showChild, setShowChild] = useState(false)
+
+  function toggleParent(){
+    setShowParent(!showParent)
+  }
+
+  function toggleChild(){
+    setShowChild(!showChild)
+  }
 
   return (
     <div>
-       
+      <button onClick={toggleParent}>Toggle Parent</button>
+      <button onClick={toggleChild}>Toggle Child</button>
+       <Parent />
+       <Child />
     </div>
   )
 }
