13f3a47cc61c33ce73c9514e5881beff2e7b1921
tanvirtbf <tanver018765@gmail.com>
2025-06-05 11:41:09 +0600

    start custom hooks

---
+++ Custom Hooks/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState(3)
+
+  function handlePlus(){
+    setValue((prevState) =>{
+      if(prevState===5){
+        return 5
+      }else {
+        return value+1
+      }
+    } )
+  }
+  function handleMinus(){
+    setValue((prevState) =>{
+      if(prevState===0){
+        return 0
+      }else {
+        return value-1
+      }
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={handlePlus}>+</button>
+      {value}
+      <button onClick={handleMinus}>-</button>
+    </div>
+  )
+}
+
+export default App
